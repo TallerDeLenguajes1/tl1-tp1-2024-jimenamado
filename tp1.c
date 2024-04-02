570f0f18b8aad5b863178e781442d31f1406beaf
jimenamado <jimenaamado12@gmail.com>
2024-04-02 00:07:54 -0300

    Punteros

--- tp1.c
+++ tp1.c
@@ -2,6 +2,17 @@
 
 void main (){
 
-    printf("Hola Mundo\n");
+    //printf("Hola Mundo\n");
+
+    int num1 = 13, *puntero;
+
+    puntero = &num1;
+
+    printf("\nEl contenido del puntero:%d",*puntero);
+    printf("\n Direccion de memoria de la variable :%p",&num1);
+    printf("\nLa direccion de memoria del puntero:%p",puntero);
+    printf("\nEl tamaño de memoria utilizado por la variable:%zu bytes", sizeof(num1));
+
+
     
 }
